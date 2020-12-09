375b2c6d905f13015a5d4bd968be0d942f5a43c7
robcrockPI <robert@propellerindustries.com>
2020-12-09 10:55:52 -0300

    My first commit

---
+++ src/components/App.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "../styles/App.css"
+// Imports for the DateRangePicker
+import "react-dates/lib/css/_datepicker.css"
+import "react-dates/initialize"
+import { DateRangePicker } from "react-dates"
+// Import component
+import Viz from "./Viz"
+
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      startDate: null,
+      endDate: null,
+      focused: null,
+    }
+  }
+
+  // Create the two handler functions to allow us to update the State when
+  // the Viz first initializes
+  setStartDate = (date) => {
+    this.setState({ startDate: date })
+  }
+
+  setEndDate = (date) => {
+    this.setState({ endDate: date })
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <DateRangePicker
+          isOutsideRange={() => false}
+          startDate={this.state.startDate} // momentPropTypes.momentObj or null,
+          startDateId="your_unique_start_date_id" // PropTypes.string.isRequired,
+          endDate={this.state.endDate} // momentPropTypes.momentObj or null,
+          endDateId="your_unique_end_date_id" // PropTypes.string.isRequired,
+          onDatesChange={({ startDate, endDate }) =>
+            this.setState({ startDate, endDate })
+          } // PropTypes.func.isRequired,
+          focusedInput={this.state.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
+          onFocusChange={(focusedInput) => this.setState({ focusedInput })} // PropTypes.func.isRequired,
+        />
+        <Viz
+          startDate={this.state.startDate}
+          endDate={this.state.endDate}
+          setStartDate={this.setStartDate}
+          setEndDate={this.setEndDate}
+        />
+      </div>
+    )
+  }
+}
---
+++ src/components/Viz.js
@@ -0,0 +1,96 @@
+import { useState, useRef, useEffect } from "react"
+import "../styles/Viz.css"
+
+const { tableau } = window
+
+export default function Viz({ startDate, endDate }) {
+  const [viz, setViz] = useState(null)
+
+  // Set up the arguments to pass into the Tableau Viz function
+  const ref = useRef(null)
+  const url =
+    "https://10ax.online.tableau.com/t/developmentonlydev595736/views/DatePicker/Dashboard"
+  const options = {
+    hideTabs: true,
+    hideToolbar: true,
+    onFirstInteractive: function() {
+      console.log("This viz is interactive.")
+    },
+  }
+
+  // This function will be run on page load to initialize our viz.
+  const initViz = () => {
+    setViz(new tableau.Viz(ref.current, url, options))
+  }
+
+  // This function will run with the startDate is changed
+  // update the parameter value in the workbook and update the viz
+  async function changeStartDateParameterValue(startDate) {
+    let startYear, startMonth, startDay
+
+    if (startDate === null) {
+      startYear = 2016
+      startMonth = 0
+      startDay = 1
+    } else {
+      startYear = startDate._d.getUTCFullYear()
+      startMonth = startDate._d.getUTCMonth()
+      startDay = startDate._d.getUTCDate()
+    }
+
+    if (viz !== null) {
+      const workbook = viz.getWorkbook()
+
+      workbook.changeParameterValueAsync(
+        "Start Date",
+        new Date(Date.UTC(startYear, startMonth, startDay))
+      )
+    } else {
+      console.log("We don't have a viz yet")
+    }
+  }
+
+  // This hook is listening to the startDate prop. When it's changed the
+  // changeStartDateParameterValue function will run with the new startDate
+  useEffect(() => {
+    changeStartDateParameterValue(startDate)
+  }, [startDate])
+
+  // This function will run with the endDate is changed
+  // update the parameter value in the workbook and update the viz
+  async function changeEndDateParameterValue(endDate) {
+    let endYear, endMonth, endDay
+
+    if (endDate === null) {
+      endYear = 2019
+      endMonth = 11
+      endDay = 31
+    } else {
+      endYear = endDate._d.getUTCFullYear()
+      endMonth = endDate._d.getUTCMonth()
+      endDay = endDate._d.getUTCDate()
+    }
+
+    if (viz !== null) {
+      const workbook = viz.getWorkbook()
+
+      workbook.changeParameterValueAsync(
+        "End Date",
+        new Date(Date.UTC(endYear, endMonth, endDay))
+      )
+    } else {
+      console.log("We don't have a viz yet")
+    }
+  }
+
+  // This hook is listening to the startDate prop. When it's changed the
+  // changeStartDateParameterValue function will run with the new startDate
+  useEffect(() => {
+    changeEndDateParameterValue(endDate)
+  }, [endDate])
+
+  // Initialize viz when the page loads
+  useEffect(initViz, [])
+
+  return <div className="viz" ref={ref} />
+}
